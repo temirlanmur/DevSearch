e4167beb626a1fa67be5513d06fe0b3df4fc6cc9
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2021-10-18 15:32:26 +0600

    Plug in Simple JWT, add token routes

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from . import views
 
@@ -8,4 +12,9 @@ urlpatterns = [
     path('', views.Routes.as_view(), name='api_routes'),
     path('projects', views.ProjectList.as_view(), name='project_list'),
     path('projects/<uuid:project_id>', views.ProjectRetrieve.as_view(), name='project_detail'),
+
+    # Simple JWT plugin's routes
+    # https://django-rest-framework-simplejwt.readthedocs.io/en/latest/index.html
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
